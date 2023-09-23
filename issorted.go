0a2536a35381aeca63bc0eec13bb37a39f082d35
The-Tifo <122095579+The-Tifo@users.noreply.github.com>
2023-09-23 04:27:49 +0300

    Add files via upload

---
+++ issorted.go
@@ -0,0 +1,17 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	ascending := true
+	descending := true
+	for i := 1; i < len(a); i++ {
+		if f(a[i-1], a[i]) > 0 {
+			ascending = false
+		}
+	}
+	for i := 1; i < len(a); i++ {
+		if f(a[i-1], a[i]) < 0 {
+			descending = false
+		}
+	}
+	return ascending || descending
+}
